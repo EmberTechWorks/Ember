8f8bd8aea0179a5d1e6245e37bcd3a6d8cb1877c
royalpinto007 <royalpinto007@gmail.com>
2023-12-18 10:52:12 +0530

    feat, style: landing page

--- client/App.js
+++ client/App.js
@@ -1,12 +1,22 @@
 import { StatusBar } from "expo-status-bar";
-import { Text, TouchableOpacity, View } from "react-native";
+import { Image, Text, TouchableOpacity, View } from "react-native";
 
 export default function App() {
   return (
-    <View className="flex-1 justify-center items-center bg-white ">
-      <TouchableOpacity className="bg-cyan-500 p-3 rounded-lg shadow-md shadow-gray-400">
-        <Text className="text-white text-3xl font-bold ">Ember</Text>
+    <View className="flex-1 justify-center items-center bg-white">
+      
+      <Image
+        source={require("./assets/ember.jpeg")}
+        style={{ width: 150, height: 150, borderRadius: 50, marginBottom: 20 }}
+      />
+      <TouchableOpacity className="rounded-lg">
+        <Text className="text-black text-5xl font-bold">ember</Text>
       </TouchableOpacity>
+
+      <TouchableOpacity className="bg-blue-500 p-2 mt-3 rounded-lg shadow-md shadow-gray-400">
+        <Text className="text-white text-2xl font-bold">Login</Text>
+      </TouchableOpacity>
+
       <StatusBar style="dark" />
     </View>
   );
